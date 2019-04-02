85bc4aa8422c42b216bbc4ad5ca2e87ada613db9
ultradcr <rparajuli4@gmail.com>
2019-04-02 21:45:57 +0545

    home page complete

--- app/src/main/java/com/example/futsalnepal/FutsalRecycleView.java
+++ app/src/main/java/com/example/futsalnepal/FutsalRecycleView.java
@@ -1,15 +1,18 @@
 package com.example.futsalnepal;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Collections;
 import java.util.List;
 
-public class FutsalRecycleView extends RecyclerView.Adapter<FutsalViewHolder> {
+public class FutsalRecycleView extends RecyclerView.Adapter<FutsalRecycleView.FutsalViewHolder> {
 
         List<Data> list = Collections.emptyList();
         Context context;
@@ -65,4 +68,31 @@ public class FutsalRecycleView extends RecyclerView.Adapter<FutsalViewHolder> {
             notifyItemRemoved(position);
         }
 
+    public class FutsalViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cv;
+        TextView name;
+        TextView address;
+        TextView time;
+        ImageView profile,add,clock,rating1,rating2,rating3,rating4,rating5;
+
+        FutsalViewHolder(View itemView) {
+            super(itemView);
+            cv =  itemView.findViewById(R.id.futsal_card_view);
+            name =  itemView.findViewById(R.id.futsal_name);
+            address = (TextView) itemView.findViewById(R.id.futsal_address);
+            time = (TextView) itemView.findViewById(R.id.futsal_available_time);
+            profile = (ImageView) itemView.findViewById(R.id.futsal_profile);
+            add = itemView.findViewById(R.id.location_icon);
+            clock = itemView.findViewById(R.id.time_icon);
+            rating1 = itemView.findViewById(R.id.futsal_rating_1);
+            rating2 = itemView.findViewById(R.id.futsal_rating_2);
+            rating3 = itemView.findViewById(R.id.futsal_rating_3);
+            rating4 = itemView.findViewById(R.id.futsal_rating_4);
+            rating5 = itemView.findViewById(R.id.futsal_rating_5);
+
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/futsalnepal/MainActivity.java
+++ app/src/main/java/com/example/futsalnepal/MainActivity.java
@@ -162,8 +162,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         List<Data> data = new ArrayList<>();
 
-        data.add(new Data("WhiteHouse", "Kapan-3","Available TIme: 6am - 6pm", R.drawable.ic_launcher_background));
-        data.add(new Data("BlackHouse", "Chabahil","Available TIme: 9am - 9pm", R.drawable.ic_launcher_background));
+        data.add(new Data("WhiteHouse", "Kapan-3","Available TIme: 6am - 6pm", R.mipmap.ic_futsal_foreground));
+        data.add(new Data("BlackHouse", "Chabahil","Available TIme: 9am - 9pm", R.mipmap.ic_futsal_foreground));
 
         return data;
     }
