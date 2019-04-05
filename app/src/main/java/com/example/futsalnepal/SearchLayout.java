ed5501ed806b991a5f05dd3919ad82555f939874
ultradcr <rprajuli4@gmail.com>
2019-04-05 17:01:11 +0545

    some search page layout

--- app/src/main/java/com/example/futsalnepal/SearchLayout.java
+++ app/src/main/java/com/example/futsalnepal/SearchLayout.java
@@ -1,13 +1,101 @@
 package com.example.futsalnepal;
 
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class SearchLayout extends AppCompatActivity {
 
+import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
+import com.borax12.materialdaterangepicker.time.TimePickerDialog;
+//import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
+
+public class SearchLayout extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private TextView dateSearch,timeSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_layout);
+        //for toolbar
+        Toolbar toolbar = findViewById(R.id.search_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        dateSearch = findViewById(R.id.date_search);
+        timeSearch = findViewById(R.id.time_search);
+
+        //time picker
+        timeSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                TimePickerDialog tpd = TimePickerDialog.newInstance(
+                        SearchLayout.this,
+                        now.get(Calendar.HOUR_OF_DAY),
+                        now.get(Calendar.MINUTE),
+                        false
+                );
+                tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        Log.d("TimePicker", "Dialog was cancelled");
+                    }
+                });
+                tpd.show(getFragmentManager(), "Timepickerdialog");
+            }
+        });
+
+//        dateSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Calendar now = Calendar.getInstance();
+//                DatePickerDialog dpd = new AlertDialog.Builder(
+//                        SearchLayout.this,
+//                        now.get(Calendar.YEAR), // Initial year selection
+//                        now.get(Calendar.MONTH), // Initial month selection
+//                        now.get(Calendar.DAY_OF_MONTH) // Inital day selection
+//                );
+//                dpd.show();
+//
+//            }
+//        });
+
     }
+    // for toolbar
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay,int minute, int hourOfDayEnd, int minuteEnd) {
+        String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+        //String minuteString = minute < 10 ? "0"+minute : ""+minute;
+        String hourStringEnd = hourOfDayEnd < 10 ? "0"+hourOfDayEnd : ""+hourOfDayEnd;
+       // String minuteStringEnd = minuteEnd < 10 ? "0"+minuteEnd : ""+minuteEnd;
+        String time = hourString+" - "+hourStringEnd;
+
+        timeSearch.setText(time);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date =dayOfMonth+" "+ month +"-"+year;
+        dateSearch.setText(date);
+    }
+
 }
