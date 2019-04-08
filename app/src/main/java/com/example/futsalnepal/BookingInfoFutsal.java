7e5b8c56cbb03b77dafe8cb6902e8a4d01bce9f6
ultradcr <rparajuli4@gmail.com>
2019-04-08 12:21:07 +0545

    some other layouts

---
+++ app/src/main/java/com/example/futsalnepal/BookingInfoFutsal.java
@@ -0,0 +1,24 @@
+package com.example.futsalnepal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class BookingInfoFutsal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_info_futsal);
+        Toolbar toolbar = findViewById(R.id.book_info_futsal_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    // for toolbar
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
