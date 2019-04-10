0676e3cefd9e7771db909d857d1ce9674b631c4d
ultradcr <rprajuli4@gmail.com>
2019-04-10 18:48:01 +0545

    Layout design finish some back logic for layout remaining

--- app/src/main/java/com/example/futsalnepal/RatingReviewFragment.java
+++ app/src/main/java/com/example/futsalnepal/RatingReviewFragment.java
@@ -1,15 +1,24 @@
 package com.example.futsalnepal;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 
 public class RatingReviewFragment extends Fragment {
+
+    private EditText mReview;
+    private Button mPost;
+    private RatingBar mRating;
     public RatingReviewFragment() {
 
     }
@@ -19,7 +28,13 @@ public class RatingReviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rating_review, container, false);
+        View view  = inflater.inflate(R.layout.fragment_rating_review, container, false);
+
+        mRating = view.findViewById(R.id.futsal_rating_input);
+        mReview = view.findViewById(R.id.review_of_futsal);
+        mPost.setBackgroundResource(R.drawable.tab_custom_shape);
+
+        return view;
     }
 }
 
