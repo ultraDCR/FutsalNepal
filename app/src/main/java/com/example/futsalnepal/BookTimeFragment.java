cde78e0193e9debe429f492baeb23c8b1fa68e3e
ultradcr <rprajuli4@gmail.com>
2019-04-10 19:49:48 +0545

    Book time recycle view some works

--- app/src/main/java/com/example/futsalnepal/BookTimeFragment.java
+++ app/src/main/java/com/example/futsalnepal/BookTimeFragment.java
@@ -3,9 +3,14 @@ package com.example.futsalnepal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +18,8 @@ import android.view.ViewGroup;
  */
 public class BookTimeFragment extends Fragment {
 
+    private RecyclerView bookTime;
+    List<BookTime> data = fill_with_data();
 
     public BookTimeFragment() {
         // Required empty public constructor
@@ -23,7 +30,34 @@ public class BookTimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_time, container, false);
+        View view = inflater.inflate(R.layout.fragment_book_time, container, false);
+
+        RecyclerView recyclerView =  view.findViewById(R.id.book_time_rview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        BookTimeViewAdapaer adapter = new BookTimeViewAdapaer(data,getContext());
+        recyclerView.setAdapter(adapter);
+
+
+        return view;
     }
 
+    public List<BookTime> fill_with_data() {
+
+        List<BookTime> data = new ArrayList<>();
+
+        data.add(new BookTime("6am", "Kapan-3"));
+        data.add(new BookTime("7am", "Chabahil"));
+        data.add(new BookTime("8am", "Chabahil"));
+        data.add(new BookTime("9am", "Chabahil"));
+        data.add(new BookTime("10am", "Chabahil"));
+        data.add(new BookTime("11am", "Chabahil"));
+        data.add(new BookTime("12am", "Chabahil"));
+        data.add(new BookTime("1pm", "Chabahil"));
+        data.add(new BookTime("2pm", "Chabahil"));
+        data.add(new BookTime("3pm", "Chabahil"));
+        data.add(new BookTime("4pm", "Chabahil"));
+
+        return data;
+    }
 }
---
+++ app/src/main/java/com/example/futsalnepal/BookTimeViewAdapaer.java
@@ -0,0 +1,103 @@
+package com.example.futsalnepal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class BookTimeViewAdapaer extends RecyclerView.Adapter<com.example.futsalnepal.BookTimeViewAdapaer.BookTimeViewHolder> {
+
+    List<BookTime> list = Collections.emptyList();
+    Context context;
+
+    public BookTimeViewAdapaer(List<BookTime> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public com.example.futsalnepal.BookTimeViewAdapaer.BookTimeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflate the layout, initialize the View Holder
+        context=parent.getContext();
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_time_single_layout, parent, false);
+        com.example.futsalnepal.BookTimeViewAdapaer.BookTimeViewHolder holder = new com.example.futsalnepal.BookTimeViewAdapaer.BookTimeViewHolder(v);
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(com.example.futsalnepal.BookTimeViewAdapaer.BookTimeViewHolder holder, int position) {
+
+        //Use the provided View Holder on the onCreateViewHolder method to populate the current row on the RecyclerView
+        holder.book_time.setText(list.get(position).book_time);
+
+
+        //animate(holder);
+
+
+        holder.bookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.bookBtn.setBackgroundResource(R.drawable.pending_button);
+                holder.bookBtn.setText("Pending...");
+                holder.bookBtn.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //returns the number of elements the RecyclerView will display
+        return list.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    // Insert a new item to the RecyclerView on a predefined position
+    public void insert(int position, BookTime data) {
+        list.add(position, data);
+        notifyItemInserted(position);
+    }
+
+    // Remove a RecyclerView item containing a specified Data object
+    public void remove(BookTime data) {
+        int position = list.indexOf(data);
+        list.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public class BookTimeViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView book_time;
+        Button bookBtn;
+
+
+
+        BookTimeViewHolder(View itemView) {
+            super(itemView);
+            book_time =  itemView.findViewById(R.id.book_time);
+            bookBtn =  itemView.findViewById(R.id.book_time_btn);
+
+
+        }
+    }
+
+}
+
