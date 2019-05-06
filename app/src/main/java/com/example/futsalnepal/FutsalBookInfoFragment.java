08ad8ff7dabcf3c8fe442b2e3bd29f7406e21309
ultradcr <rparajuli4@gmail.com>
2019-05-06 10:28:54 +0545

    pradeep works merge

---
+++ app/src/main/java/com/example/futsalnepal/FutsalBookInfoFragment.java
@@ -0,0 +1,42 @@
+package com.example.futsalnepal;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FutsalBookInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_futsal_book_info, container, false);
+
+//        Toolbar toolbar = view.findViewById(R.id.book_information_toolbar);
+//        view.setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //View Pager
+        ViewPager viewPager =  view.findViewById(R.id.book_information_vpager);
+        FutsalBookInformationPagerAdapter fPagerAdapter = new FutsalBookInformationPagerAdapter(this.getActivity(), getSupportFragmentManager());
+        viewPager.setAdapter(fPagerAdapter);
+        TabLayout tablayout =  view.findViewById(R.id.book_information_tablayout);
+        tablayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    // for toolbar
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        onBackPressed();
+//        return true;
+//    }
+}
--- app/src/main/java/com/example/futsalnepal/FutsalHome.java
+++ app/src/main/java/com/example/futsalnepal/FutsalHome.java
@@ -1,13 +1,96 @@
 package com.example.futsalnepal;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class FutsalHome extends AppCompatActivity {
+
+
+    private BottomNavigationView bNav;
+    private FrameLayout bNavFrame;
+
+    private FutsalBookInfoFragment bookInformation;
+    private FutsalBookNowFragment bookNow;
+    private FutsalProfile fProfile;
+    private FutsalRatingReview fRatingReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_futsal_home);
+        bNav = findViewById(R.id.futsal_nav);
+        removePaddingFromNavigationItem();
+
+
+        bNavFrame = findViewById(R.id.futsal_frame);
+
+        bookInformation = new FutsalBookInfoFragment();
+        bookNow = new FutsalBookNowFragment();
+        fProfile = new FutsalProfile();
+        fRatingReview = new FutsalRatingReview();
+
+
+        setFragment(bookInformation);
+
+        bNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.nav_book_info:
+                        setFragment(bookInformation);
+                        //setFragment(bookInformation);
+                        return true;
+
+                    case  R.id.nav_book_now:
+                        setFragment(bookNow);
+                        return true;
+
+                    case  R.id.nav_profile:
+                        setFragment(fProfile);
+                        return true;
+
+                    case R.id.nav_rating_review:
+                        setFragment(fRatingReview);
+                        return true;
+
+                    default:
+                        return false;
+
+                }
+            }
+        });
     }
+
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTran = getSupportFragmentManager().beginTransaction();
+        fragmentTran.replace(R.id.futsal_frame, fragment);
+        fragmentTran.commit();
+
+    }
+
+    public void removePaddingFromNavigationItem() {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bNav.getChildAt(0);
+
+        for (int i = 0; i < menuView.getChildCount(); i++) {
+            BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+            View activeLabel = item.findViewById(R.id.largeLabel);
+            if (activeLabel instanceof TextView) {
+                activeLabel.setPadding(0, 0, 0, 0);
+            }
+        }
+    }
+
 }
